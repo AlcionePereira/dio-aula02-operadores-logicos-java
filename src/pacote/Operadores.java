f68a931b3f2eabea072b47d63fb1231507317b36
Alcione Pereira <catce.2021211mtds0453@gmail.com.ifpi.edu>
2022-07-15 06:24:18 -0300

    Aula 02 operadores lógicos.

---
+++ src/pacote/Operadores.java
@@ -0,0 +1,25 @@
+package pacote;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        int il = 10;
+        int i2 = 20;
+        float f1 = 4.5f;
+        float f2 = 3.5f;
+        double d1 = 59.6d;
+        char c1 = 'x';
+        char c2 = 'y';
+        String s1 = "Fulano";
+        String  s2 = "cicrano";
+        boolean b1 = true;
+        boolean b2 = false;
+
+        long l1 = 1597l;
+        long l2 = 8997l;
+        byte y1 = 1;
+        short h1 = 25;
+
+        System.out.println("");
+    }
+}
